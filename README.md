af3265d6dfaf7fd48d3b5c99b96e89c17417003a
System <none>
2017-08-19 13:56:38 +0000

    Added JS Standard Code Style Badge to Readme

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A microservice which reads your request headers and returns JSON containing your client IP address, language and software.
 
 Test it at <a href="https://a-request-header-parser-microservice.glitch.me/">https://a-request-header-parser-microservice.glitch.me/</a>
+
+<img src="https://cdn.glitch.com/901560a2-0c58-4bb2-868b-43c0ac5d1d10%2Fjsstandard-badge.svg?1503150814326" alt="JSStandard Badge" height="56" width="171">
--- server.js
+++ server.js
@@ -1,39 +1,35 @@
-'use strict';
+'use strict'
 
-let fs = require('fs');
-let express = require('express');
-let app = express();
-let requestHeaderParser = require("./lib/requestHeaderParser.js");
+const express = require('express')
+const app = express()
+const requestHeaderParser = require('./lib/requestHeaderParser.js')
 
+app.use('/public', express.static(process.cwd() + '/public'))
 
-app.use('/public', express.static(process.cwd() + '/public'));
-  
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    });
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
 // Request Header Parser
-app.get('/api/who-are-you', function(req, res) {
-  res.send(requestHeaderParser.whoAreYou(req.headers));
-});
+app.get('/api/who-are-you', function (req, res) {
+  res.send(requestHeaderParser.whoAreYou(req.headers))
+})
 
 // Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
-});
+app.use(function (req, res, next) {
+  res.status(404)
+  res.type('txt').send('Not found')
+})
 
 // Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
+app.use(function (err, req, res, next) {
+  if (err) {
     res.status(err.status || 500)
       .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
+      .send(err.message || 'SERVER ERROR')
+  }
 })
 
 app.listen(process.env.PORT, function () {
-  console.log('Node.js listening on port ' + process.env.PORT);
-});
-
+  console.log('Node.js listening on port ' + process.env.PORT)
+})
